a4a2fd5cae4c60c0b99c78bf300f1c241a93fd71
Ryan Bahan <ryan.bahan@shopify.com>
2021-12-09 10:28:00 -0700

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 echo 'HIIIIIIIIIIIIIIII ASDFF 1'
-if [[ "$SPIN" = 1 ]]
+if [ "$SPIN" = 1 ]
 echo 'HIIIIIIIIIIIIIIII ASDFF 22'
 then
-  if [[ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]]
+  if [ "$SPIN_REPO_SOURCE_PATH" = "/src/github.com/shopify/shopify" ]
   then
     echo 'HIIIIIIIIIIIIIIII ASDFF 33'
     cartridge insert ryancbahan/enable-sp-and-balance-beta-flag
