dc9dcbfec9d6274cc77b83dd9e4b99f428ea39f7
Ryan Bahan <ryan.bahan@shopify.com>
2021-12-09 10:53:44 -0700

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,7 @@
 echo '1'
 if [ "$SPIN" = 1 ]
-echo '2'
-pwd
-cat pwd
 then
-  if [ "$SPIN_REPO_SOURCE_PATH" = "/home/spin/src/github.com/Shopify/shopify" ]
-  then
-    echo '3'
-  fi
+  echo '2'
+  cd '/home/spin/src/github.com/Shopify/shopify'
+  pwd
 fi
