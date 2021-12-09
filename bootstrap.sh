c6cabbc658d757232de7b6f3de0ff5b429ae4cf6
Ryan Bahan <ryan.bahan@shopify.com>
2021-12-09 12:02:21 -0700

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,4 @@ then
   echo '2'
   cd '/home/spin/src/github.com/Shopify/shopify'
   pwd
-  rake dev:betas:enable BETA=shopify_banking SHOP_ID=1
-  bin/rake dev:shopify_payments:setup COUNTRY=US SHOP_ID=1
 fi
